bd446647cdd0801fe4316b37d191898e53731ba2
AltGrF13 <AltGrF13@gmail.com>
2024-10-25 01:34:31 +0300

    Объединение указанных плейлистов

--- TV-resave_on_Windows.cmd
+++ TV-resave_on_Windows.cmd
@@ -1,5 +1,19 @@
-cd /d d:\Project\LoganetXIPTV
+@echo off
+set "loganet_path=d:\Project\LoganetXIPTV"
+set "loganet_playlists=LoganetXMovie LoganetXInfo"
+set "final_file=\\ROUTER\Flash\IPTV.m3u8"
 
+cd /d %loganet_path%
 git pull
+echo/
 
-copy .\LoganetXAll.m3u \\ROUTER\Flash\IPTV.m3u8 /y
+set /p extm3u=< %loganet_path%\LoganetXAll.m3u
+>%final_file% echo %extm3u%
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+(for %%n in (%loganet_playlists%) do (
+    set "playlist_path=%loganet_path%\%%n.m3u"
+    for /f "skip=1 delims=" %%r in (!playlist_path!) do (
+        echo %%r
+    )
+))>>%final_file%
