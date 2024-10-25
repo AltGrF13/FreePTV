83fda307724bf331a6a2c252480e0a583d9e311f
AltGrF13 <AltGrF13@gmail.com>
2024-10-25 06:28:01 +0300

    Проверка наличия зависимого файла

--- TV-resave_and_run_on_Windows.cmd
+++ TV-resave_and_run_on_Windows.cmd
@@ -1,5 +1,14 @@
 @echo off
+set "resave_filename=TV-resave_on_Windows.cmd"
 
-call %~dp0TV-resave_on_Windows.cmd
+set "resave_file=%~dp0%resave_filename%"
+if not exist %resave_file% (
+	echo: Download https://raw.githubusercontent.com/AltGrF13/FreePTV/refs/heads/main/%resave_filename%
+	echo: in %~dp0
+	echo: and try again
+
+	exit /b 1
+)
+call %resave_file%
 
 start %final_file%
