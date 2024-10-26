4ff6ca709f03c9fced5bdfefc413760a18676872
AltGrF13 <AltGrF13@gmail.com>
2024-10-26 03:07:36 +0300

    Поплейлистный многостраничный вывод

--- TV-resave_on_Windows.cmd
+++ TV-resave_on_Windows.cmd
@@ -1,8 +1,10 @@
 @echo off
 set "loganet_path=d:\Project\LoganetXIPTV"
-set "loganet_playlists=LoganetXAll"
+set "playlists=LoganetXAll"
 set "black_list=Общие Детские Новости Политические Хобби Религия Спорт Музыка"
 set "final_file=\\192.168.1.1\Flash\IPTV.m3u8"
+set LB=^
+
 
 cd /d %loganet_path%
 git pull
@@ -12,13 +14,13 @@ rem #EXTM3U from LoganetXAll
 set /p extm3u=< %loganet_path%\LoganetXAll.m3u
 >%final_file% echo %extm3u%
 rem echo without newline
-<nul set /p =%final_file% created
+<nul set /p =%final_file% is created
 
-set /a "channels_count=0"
 SETLOCAL ENABLEDELAYEDEXPANSION
-(for %%n in (%loganet_playlists%) do (
+for %%n in (%playlists%) do (
+	set /a "channels_count=0"
 	set "playlist_path=%loganet_path%\%%n.m3u"
-	for /f "skip=1 delims=" %%r in (!playlist_path!) do (
+	(for /f "skip=1 delims=" %%r in (!playlist_path!) do (
 		set "row=%%r"
 
 		if "!row:~0,7!"=="#EXTINF" (
@@ -46,6 +48,8 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 		if defined is_filtered (
 			echo:%%r
 		)
-	)
-))>>%final_file%
-echo: and filled with !channels_count! channels
+	))>>%final_file%
+
+	<nul set /p =,!LB! filled with !channels_count! high-quality channels from "!playlist_path!"
+)
+echo:.
