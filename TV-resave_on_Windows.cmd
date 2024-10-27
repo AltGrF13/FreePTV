fb401f9f4ef50e578859bd6628eac9baabacf5d0
AltGrF13 <AltGrF13@gmail.com>
2024-10-27 04:13:20 +0300

    #1 Избранное

--- TV-resave_on_Windows.cmd
+++ TV-resave_on_Windows.cmd
@@ -1,6 +1,7 @@
 @echo off
 set "LOGANET_PATH=d:\Project\LoganetXIPTV"
 set "PLAYLISTS=LoganetXAll"
+set FAVORITES="Viju Nature HD","Animal Planet HD"
 set "BLACK_LIST=Общие Детские Новости Политические Хобби Религия Спорт Музыка"
 set "FINAL_FILE=\\192.168.1.1\Flash\IPTV.m3u8"
 set LB=^
@@ -17,6 +18,35 @@ rem echo without newline
 <nul set /p =%FINAL_FILE% is created
 
 SETLOCAL ENABLEDELAYEDEXPANSION
+
+for %%n in (%PLAYLISTS%) do (
+	set /a "channels_count=0"
+	set "playlist_path=%LOGANET_PATH%\%%n.m3u"
+	(for /f "skip=1 delims=" %%r in (!playlist_path!) do (
+		set "row=%%r"
+
+		if "!row:~0,7!"=="#EXTINF" (
+			set "row=!row:&=^&!"
+			set "is_favorites="
+			for %%c in (%FAVORITES%) do (
+				set "str=%%c"
+				echo:!row!| findstr /i /c:"!str:~1,-1!" >nul 2>&1 && (
+					set "is_favorites=true"
+				)
+			)
+
+			if defined is_favorites (
+				set /a "channels_count=!channels_count!+1"
+			)
+		)
+
+		if defined is_favorites (
+			echo:%%r
+		)
+	))>>%FINAL_FILE%
+
+	<nul set /p =,!LB! filled with !channels_count! favorites channels from "!playlist_path!"
+)
 for %%n in (%PLAYLISTS%) do (
 	set /a "channels_count=0"
 	set "playlist_path=%LOGANET_PATH%\%%n.m3u"
